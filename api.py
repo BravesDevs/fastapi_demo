30231c2e6df3902fb0610a1d4dc075ec4caec154
Devang Parekh <devang.parekh@marutitech.com>
2022-06-17 20:26:13 +0530

    Add User crud API

--- api.py
+++ api.py
@@ -1,26 +1,84 @@
-from typing import Optional
-from datetime import date as date_type
-from fastapi import FastAPI, File, UploadFile, Datetime
-from utils.utils import convertBytesToString, parsed_csv
+from typing import List
 
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
+import crud
+from models import models, schemas
+from config.db import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get('/')
-def getName():
-    return {'ok': True, 'content': "Hello World"}
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
-@app.post('/csv/')
-async def parse_csv(file: UploadFile = File(...)):
-    contents = await file.read()
-    json_string = convertBytesToString(contents)
-    return {
-        'ok': True,
-        'content': json_string
-    }
+@app.post("/users/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
 
 
-# @app.post('/calculate_age/')
-# def calculateAge(date:date_type):
+@app.get("/users/", response_model=List[schemas.User])
+def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = crud.get_users(db, skip=skip, limit=limit)
+    return users
+
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+def read_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@app.post("/users/{user_id}/items/", response_model=schemas.Item)
+def create_item_for_user(
+    user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_user_item(db=db, item=item, user_id=user_id)
+
+
+@app.get("/items/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
+
+
+# from typing import Optional
+# from datetime import date as date_type
+# from fastapi import FastAPI, File, UploadFile, Datetime
+# from utils.utils import convertBytesToString, parsed_csv
+
+
+# app = FastAPI()
+
+
+# @app.get('/')
+# def getName():
+#     return {'ok': True, 'content': "Hello World"}
+
+
+# @app.post('/csv/')
+# async def parse_csv(file: UploadFile = File(...)):
+#     contents = await file.read()
+#     json_string = convertBytesToString(contents)
+#     return {
+#         'ok': True,
+#         'content': json_string
+#     }
+
+
+# # @app.post('/calculate_age/')
+# # def calculateAge(date:date_type):
