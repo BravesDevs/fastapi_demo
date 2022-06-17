19d0d56341ab4c7be46097dec2515fbf0fb4d004
Devang Parekh <devang.parekh@marutitech.com>
2022-06-17 20:52:22 +0530

    Change the sqlite path

--- config/db.py
+++ config/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///../sql_app.sqlite"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.sqlite"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={
                        "check_same_thread": False})
 
